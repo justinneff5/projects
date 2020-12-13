28e34ee18f536998f49ae7cd20c2f2026029919d
tejas.mattur <tejas.mattur@wustl.edu>
2020-12-12 16:51:44 -0800

    g

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const mod7_model = require('./mod7_model')
 app.use(express.json())
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,OPTIONS'); //got rid of delete after put
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
 });
@@ -52,4 +52,3 @@ app.post('/users', (req, res) => {
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
-
