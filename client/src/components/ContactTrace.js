6fb0ff1a113539dbd9b2ce442996e7d5094648b9
justinneff5 <59897146+justinneff5@users.noreply.github.com>
2020-12-14 23:32:37 -0800

    holy crud

--- client/src/components/ContactTrace.js
+++ client/src/components/ContactTrace.js
@@ -1,95 +1,87 @@
 import React, { Fragment, useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 
-function compileDataArr(data) {
+function Connection(userOne, userTwo, userTwoStatus) {
+  this.user1 = userOne;
+  this.user2 = userTwo;
+  this.user2status = userTwoStatus;
+}
+
+function compileDataArr(data) { //take second parameter x username
   let retArr = [];
   let i;
   let count = Object.keys(data).length;
   for (i = 0; i < count; i++) {
-      retArr.push(data[i].user2);
+      retArr.push(new Connection(data[i].user1, data[i].user2, data[i].user2status));
+      
       }
-  return retArr;
+    return retArr;
 }
 
-function compileDataStatus(data) {
-    let retStr;
-    retStr = data.status;
-    return retStr;
+
+///GET CACHE
+
+const getCache = async() => {
+  // function getCache(){
+    try {
+      let u2status = "positive"; // change back to positive
+      let x;
+      const res = await fetch('http://localhost:3001/other/cacheDatabase', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({status : u2status}),
+      });
+
+      const r = await res.json();
+      //console.log(r);
+      x = compileDataArr(r);
+      console.log(x);
+      return x;
+      //Promise.resolve(x);
+      // store(x);
+      //console.log(x[0].user1);
+      //return x;
+      // if (res.status === 200) {
+      //   console.log("sup");
+      // }
+      //this.setState({Cache: x});
+    }
+    catch (err) {
+       console.error(err.message);
+    }
+    //return getCache();
+    
+    // .then(response => response.json())
+    // .then(data => {
+    //     x = compileDataArr(data);
+    //     console.log(data);
+    //     console.log(x);
+        //return getCache;
+        //return retDat;
+        // retDat = await res.json();
+        // dat = compileDataArr(retDat);
+        // return dat;
 }
 
-// function compileDataRisk(data) {
-//     let retStr = JSON.parse(data).risk;
-//     return retStr;
-// }
 
-function getAllConnections(username, homeusername, primusername) {
-    let user = username;
-    let home = homeusername;
-    let prim = primusername;
-    let retDat;
-    fetch('http://localhost:3001/other/getAllConnections', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({user1 : user, homeuser : home, primuser : prim}),
-    })
-    .then(response => response.json())
-    .then(data => {
-        retDat = compileDataArr(data);
-        return retDat;
-      //getUsers();
-    });
-}
 
-  function getStatus(username) {
-    let user = username;
-    let retDat;
-    fetch('http://localhost:3001/authentication/getStatus', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({user}),
-    })
-    .then(response => response.json())
-    .then(data => {
-        retDat = compileDataStatus(data);
-        return retDat;
-    });
-}
-
-//   function getRisk(username){
-//     let user = username;
-//     fetch('http://localhost:3001/authentication/getRisk', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({user}),
-//     })
-//       .then(response => response.json())
-//       .then(data => {
-//         retDat = compileDataRisk(data);
-//         return retDat;
-//       });
-//   }
-
-  function updateRisk(username) {
-    let user = username;
+  function updateRisk(userUp) { //this updates to positive
+    let user = userUp;
+    let riskStatus = 'some';
     fetch('http://localhost:3001/authentication/risk', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({user}),
+      body: JSON.stringify({username : user, risk : riskStatus}),
     })
-    .then(response => response.json())
     .then(data => {
       console.log(data);
       //getUsers();
     });
-}
+  }
 
 
 
@@ -103,99 +95,122 @@ function getAllConnections(username, homeusername, primusername) {
 //ES6 -> arrow functions
 //parameter => 
 //
+  function networkStatus(num, pos) {
+    let str;
+    if (num == 0) {
+      str = "Wow! You have 0 connections. Great job, you are being very safe!";
+    }
+    else if (num < 20 && num > 0) {
+      str = "! You have " + num + " connections to the tertiary level. You're being relatively safe. Your network status is yellow!";
+    }
+    else if (num > 20 && num < 40) {
+      str = "!! You have " + num + " connections to the tertiary level. You're being pretty unsafe. Try to cut down on social interactions a bit, or talk to the people you see about being safer. Your network status is orange";
+    }
+    else {
+      str = "!!! You have " + num + " connections to the tertiary level. You're being very unsafe. Try to cut down on social interactions a lot, or talk to the people you see about being MUCH safer. Your network status is red";
+    }
+    if (pos === 0) {
+      str += " However, you have not come into contact with a positive user.";
+    }
+    return str;
+  }
+  
 
-//   const contactTrace = ({username}) => {
-// //     const [myContactTrace, updateContactTrace] = useState(username)
-//     const onSubmitForm = async e => {
-//         e.preventDefault();
-//         try {
 
-
-    function contactTrace(username){
+  async function contactTrace(username){
       let user = username;
+      console.log(user);
+      let cache = await getCache();
+      console.log("flag");
+      console.log(cache);
       let primary = [];
       let secondary = [];
-      let tertiary = [];
       let notifications = [];
       let count = 0;
       let riskCheck = 0;
       let i;
       let j;
-      let k;
       
-      primary = getAllConnections(user, user, user);
-      for (i = 0; i < primary.length; i++){
-        count++;
-        secondary.length = 0; //resets second array
-        if (getStatus(primary[i]) === "positive") {
-          notifications.push("You came in contact with " + primary[i] + " who has tested positive for covid. You need to get tested!");
-          riskCheck = 1;
+    //notifications.push("You came in contact with " + primary[i] + " who has tested positive for covid. You need to get tested!");
+
+      for (i = 0; i < cache.length; i++) { //set up primary connections
+        //console.log()
+        if(cache[i].user1 === user) { //all conn where main user is user1
+          primary.push(cache[i].user2);
+          count++;
+          if (cache[i].user2status === "positive") {
+            notifications.push("You came in primary contact with " + cache[i].user2 + " who has tested positive for covid. You need to get tested!");
+            riskCheck = 1;
+          }
         }
-        // else if (getRisk(primary[i]) === "yes") {
-        //   notifications.push("You came in contact with " + primary[i] + " who is at risk of being positive for covid. You need to get tested!");
-        //   riskCheck = 1;
-        // }
-        secondary = getAllConnections(primary[i], user, primary[i]);
-          for (j = 0; j < secondary.length; j++){
+        
+      }
+      for (i = 0; i < cache.length; i++) { //set up secondary connections
+        for (j = 0; j < primary.length; j++) { //all conn where primary connection is user1
+          if(cache[i].user1 === primary[j] && primary[j] != user) {
+            secondary.push(cache[i].user2);
             count++;
-            tertiary.length = 0; //resets tert array
-            if (getStatus(secondary[j]) === "positive") {
-              notifications.push("You came in secondary contact with " + secondary[j] + " who has tested positive for covid. You need to get tested!");
+            if (cache[i].user2status === "positive") {
+              notifications.push("You came in secondary contact with " + cache[i].user2 + " who has tested positive for covid. You need to get tested!");
               riskCheck = 1;
             }
-            // else if (getRisk(secondary[j]) === "yes") {
-            //   notifications.push("You came in secondary contact with " + secondary[j] + " who is at risk of being positive for covid. You need to get tested!");
-            //   riskCheck = 1;
-            // }
-            tertiary = getAllConnections(secondary[j], user, primary[i]);
-              for (k = 0; k < tertiary.length; k++){
-                count++;
-                if (getStatus(tertiary[k]) === "positive") {
-                  notifications.push("You came in tertiary contact with " + secondary[j] + " who has tested positive for covid. You need to get tested!");
-                  riskCheck = 1;
-                }
-                // else if (getRisk(tertiary[k]) === "yes") {
-                //   notifications.push("You came in tertiary contact with " + secondary[j] + " who is at risk of being positive for covid. You need to get tested!");
-                //   riskCheck = 1;
-                // }
-            }
+          }
         }
       }
+
+      for (i = 0; i < cache.length; i++) { //set up tertiary connections
+        for (j = 0; j < secondary.length; j++) { 
+          if(cache[i].user1 === secondary[j] && secondary[j] != user) {
+            count++;
+            if (cache[i].user2status === "positive") {
+              notifications.push("You came in tertiary contact with " + cache[i].user2 + " who has tested positive for covid. You need to get tested!");
+              riskCheck = 1;
+            }
+            j = secondary.length; //removes chance of dupes
+          }
+        }
+      }
+
+      
       if (riskCheck === 1) {
         updateRisk(user);
       }
+      //console.log(networkStatus(count, notifications.length));
+      notifications.unshift(networkStatus(count, notifications.length));
+      console.log("flag-notifications");
+      console.log(notifications);
       return notifications;
-    //   notifications.forEach(function(item){
-    //     //document.getElementById("notifications").innerHTML = document.getElementById("notifications").innerHTML + "<br>" + item + "<br>";
-    //   }) //probably need to populate innerhtml w this
-
-    
-
-
-    // return (
-    //     <Fragment>
-    //       <h3 className="mt-5">Contact Trace</h3>
-    //       <form onSubmit={onSubmitForm}>
-    //         <input
-    //           type="radio"
-    //           checked = {myStatus === "positive"}
-    //           name="positive"
-    //           value="positive"
-    //           onChange={e => updateMyStatus("positive")}
-    //           className="form-control my-3"
-    //         /> Positive
-    //         <input
-    //           type="radio"
-    //           checked = {myStatus === "negative"}
-    //           name="negative"
-    //           value="negative"
-    //           onChange={e => updateMyStatus("negative")}
-    //           className="form-control my-3"
-    //         /> Negative
-    //         <button className="btn btn-success btn-block">Submit</button>
-    //       </form>
-    //     </Fragment>
-    //   );
   }
 
- //document.getElementById("contacttrace").addEventListener("click", contactTrace, false);
+  //look at documentation to set state to notif
+
+  const ContactTrace = ({updateContactTrace}) => {
+    //const [myContactTrace, updateContactTrace] = useState(username)
+    //use setState if this doesn't work
+    const [notif, setNotif] = useState([]);
+    //const [retDat, setRetDat] = useState([]);
+      const onSubmitForm = async e => {
+          e.preventDefault();
+          const ct = await contactTrace(updateContactTrace);
+          setNotif(ct);
+          console.log(ct);
+          console.log("flag-notif");
+          console.log(notif);
+      }
+    return (
+      //html here
+      <Fragment>
+        <div className = "CT">
+          <div className = "populated">
+            {/* {notif.forEach(function(x){
+              return <li> {x} </li>; //should return html elements
+            }) */}
+            {notif.map((x) => x)}
+          </div>
+          <button className = "btn btn-success btn-block" onClick = {onSubmitForm}> Contact Trace! </button>
+        </div>
+      </Fragment>
+    )
+    }
+
+    export default ContactTrace;
